83937277206ce711812221f66f35c935cfd0dc29
ippsav <mehdiboujid@gmail.com>
2024-05-21 18:11:17 +0100

    fix

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
-// Uncomment this block to pass the first stage
-// const net = std.net;
+const net = std.net;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
@@ -8,19 +7,17 @@ pub fn main() !void {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     try stdout.print("Logs from your program will appear here!", .{});
 
-    // Uncomment this block to pass the first stage
-    //
-    // const address = try net.Address.resolveIp("127.0.0.1", 6379);
-    //
-    // var listener = try address.listen(.{
-    //     .reuse_address = true,
-    // });
-    // defer listener.deinit();
-    //
-    // while (true) {
-    //     const connection = try listener.accept();
-    //
-    //     try stdout.print("accepted new connection", .{});
-    //     connection.stream.close();
-    // }
+    const address = try net.Address.resolveIp("127.0.0.1", 6379);
+
+    var listener = try address.listen(.{
+        .reuse_address = true,
+    });
+    defer listener.deinit();
+
+    while (true) {
+        const connection = try listener.accept();
+
+        try stdout.print("accepted new connection", .{});
+        connection.stream.close();
+    }
 }
